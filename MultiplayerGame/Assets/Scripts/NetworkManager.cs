e4cba2c1631fd4b53158a344b23a1d6e719b002d
Tommi Juutilainen <juutilainen.tommi@gmail.com>
2020-02-04 23:19:25 +0200

    Initial Commit
    Adding 3D photon multiplayer unity project to github.

---
+++ MultiplayerGame/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : Photon.PunBehaviour
+{
+    private const string roomName = "RoomName";
+    private RoomInfo[] roomsList;
+    public List<PhotonPlayer> currentPlayersInRoom = new List<PhotonPlayer>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.logLevel = PhotonLogLevel.ErrorsOnly;
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        //Ruudun vasemmassa ylänurkassa tarkempaa tietoa yhteyden tilasta
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+
+        //Jos ei olla missään huonessa, eli lobbyssa, näytetään nappuloita huoneista
+        if (PhotonNetwork.room == null)
+        {
+            //Ei olla huoneessa, tehdään huoneenluomisnappula
+            if(GUI.Button(new Rect(100, 100, 250, 100), "Start Server (Create Room)"))
+            {
+                //Luodaan jokaiselle huoneelle satunnainen nimi
+                PhotonNetwork.CreateRoom(roomName + System.Guid.NewGuid().ToString("N"));
+            }
+
+            //Huoneisiin liittyminen
+            if(roomsList != null)
+            {
+                for(int i = 0; i < roomsList.Length; i++)
+                {
+                    if (GUI.Button(new Rect(100, 250 +(110*i), 250, 100), "Join " + roomsList[i].Name + 
+                        "\n\nCount: " + roomsList[i].PlayerCount))
+                    {
+                        PhotonNetwork.JoinRoom(roomsList[i].Name);
+                    }
+                }
+            }
+        }
+    }
+
+    public override void OnConnectedToPhoton()
+    {
+        Debug.Log("Yhteys Photoniin");
+    }
+
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Tultiin Lobbyyn");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Masteryhteys");
+    }
+
+    public override void OnReceivedRoomListUpdate()
+    {
+        //Jos huonelistaus päivittyy palvelimella, tämä metodi ajetaan.
+        //Jos siis joku tekee huoneen tai joku huone menee tyhjäksi ja poistuu olemasta.
+        roomsList = PhotonNetwork.GetRoomList();
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Huone Tehty");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("PlayerBox", new Vector3(0, 0.5f, 0), Quaternion.identity, 0);
+        Debug.Log("Tultiin Huoneeseen");
+    }
+
+}
---
+++ MultiplayerGame/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : Photon.MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotateSpeed;
+    public float gunRotateSpeed;
+    public GameObject gunRotator;
+    public GameObject ammo;
+    public GameObject ammoSpawn;
+    public Camera myCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (photonView.isMine)
+        {
+            myCam.enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.isMine) { 
+            //Pelaajan liike
+            float xMovement = Input.GetAxis("Horizontal");
+            float zMovement = Input.GetAxis("Vertical");
+            transform.Translate(xMovement * moveSpeed * Time.deltaTime, 0, zMovement * moveSpeed * Time.deltaTime);
+
+            //Pelaajan kääntö
+            float xMouse = Input.GetAxis("Mouse X");
+            Vector3 lookAt = new Vector3(0, xMouse * rotateSpeed * Time.deltaTime, 0);
+            transform.Rotate(lookAt);
+
+            //Gunin kääntö ylös alas
+            float yMouse = Input.GetAxis("Mouse Y");
+            Vector3 aimAt = new Vector3(yMouse * gunRotateSpeed * Time.deltaTime * -1, 0, 0);
+            gunRotator.transform.Rotate(aimAt);
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+                photonView.RPC("Shoot", PhotonTargets.Others);
+            }
+        } //photonView.isMine
+    }
+    [PunRPC]
+    public void Shoot()
+    {
+        GameObject ammoInstance = Instantiate(ammo, ammoSpawn.transform.position,
+            Quaternion.identity);
+        ammoInstance.GetComponent<Rigidbody>().AddForce(gunRotator.transform.forward * 20,
+            ForceMode.Impulse);
+    }
+
+    /*
+     -Tehtävä: Tee jokaiselle pelaajalle health-arvo. Kun pelaaja osuu toisen pelaajan palloon (ei omaansa),
+        vähennä healthiä esim 10.
+     -Laita jokaisen pelaajan päälle tekstikenttä, jossa näkyy tämänhetkinen health.
+     -Laita tykki eli gunRotator kääntymään myös muilla pelaajilla.
+    */
+}
